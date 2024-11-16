237de6d268523c34b252cc72eef68dd958f264be
ARKA BASAK <sepic466@gmail.com>
2024-11-16 18:03:23 +0530

    minor change

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 
 const app = express();
 
-
+ 
 app.use(express.json());
 app.use(cors())
 app.get("/", (req, res) => res.send("Express on Vercel"));
@@ -24,7 +24,8 @@ app.post('/api/v1/manga', async(req, res)=>{
             method: 'GET',
             url: `${baseUrl}/manga`,
             params: {
-                title: query
+                title: query,
+                limit:25
             }
         });
 
@@ -35,6 +36,26 @@ app.post('/api/v1/manga', async(req, res)=>{
         res.status(400).send({error: error})
     }
 
+})
+
+app.post('/api/v1/chapter', async(req,res)=>{
+    const {id} = req.body
+    const url = `https://api.mangadex.org/manga/${id}/feed?translatedLanguage%5B%5D=en&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&includeFutureUpdates=1&order%5BcreatedAt%5D=asc&order%5BupdatedAt%5D=asc&order%5BpublishAt%5D=asc&order%5BreadableAt%5D=asc&order%5Bvolume%5D=asc&order%5Bchapter%5D=asc`
+
+
+    const baseUrl = 'https://api.mangadex.org';
+
+    const resp = await axios({
+        method: 'GET',
+        url: `${baseUrl}/manga/${id}/feed`,
+        params: {
+            translatedLanguage: ['en']
+        }
+    });
+
+    // const resp = await axios.get(url)
+    // chaplist = resp.data.data
+    res.status(200).send({msg:"manga fetched success" , data:resp.data.data})
 })
 
   
