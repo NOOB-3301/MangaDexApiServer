5415a01b2c49d37adfa7b6f6b0510c526559be81
ARKA BASAK <sepic466@gmail.com>
2024-11-19 00:16:37 +0530

    addded some configuratins cors3

--- api/index.js
+++ api/index.js
@@ -109,9 +109,7 @@ app.post('/api/v1/top-anime',async(req,res)=>{
 })
 
 app.post('/api/v1/ep-link',async (req,res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Or specify your front-end domain
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
     fetchStreamingLink(req,res)
 })
 
